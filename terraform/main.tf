c813f3552be67b0a9d05dd0bb8d6a5d6574cbe74
sravani110 <sravanivangala889@gmail.com>
2026-04-12 11:48:38 +0530

    modified main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,27 @@
+terraform {
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "vault" {
+  address = "http://127.0.0.1:8200"
+}
+
+data "vault_kv_secret_v2" "secret-creds" {
+  mount = "secret"
+  name  = "cloud_user"
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = data.vault_kv_secret_v2.secret-creds.data["access_key"]
+  secret_key = data.vault_kv_secret_v2.secret-creds.data["secret_key"]
+}
+
 module "ec2module" {
   source             = "./modules/ec2"
   instance_type      = var.instance_type
